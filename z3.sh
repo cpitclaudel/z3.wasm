84b8f4a2879f7d955747bd768dfd998c1e2b672b
Clément Pit-Claudel <clement.pitclaudel@live.com>
2018-04-15 00:56:31 -0400

    all: Add license headers

--- z3.sh
+++ z3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2016 Clément Pit-Claudel
+# Copyright (c) 2017 Clément Pit-Claudel
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
